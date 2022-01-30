c71acabcda8ac4f97e86d5b37bc7f62024864249
Anish Tewary <anishtewary@anisMac.local>
2022-01-30 16:31:24 +0530

    about us page updated

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,14 @@
+class LandingController < ApplicationController
+
+  before_action :authenticate_user!, :except => [:index]
+  respond_to :json, :html
+  # layout :determine_layout
+  
+  def index
+  end
+
+  def about_us
+    user = current_user
+    @email = user.email
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'home/about_us'
+  get 'home/about_us' => "home#about_us", :as => :user_root
 end
